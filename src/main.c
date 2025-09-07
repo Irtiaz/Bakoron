67a2a6f5e1fc03407319c860e28d04f5b9058446
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2025-09-07 23:52:27 +0600

    Bakoron user interface planned out

--- src/main.c
+++ src/main.c
@@ -1,6 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef enum { EXPRESSION, BIT, PLUS } Symbol;
+
+size_t get_next_token(const char *string, int *token, void *user_data) {
+  (void)user_data;
+
+  if (string[0] == '+') {
+    *token = PLUS;
+    return 1;
+  }
+
+  else if (string[0] == '1') {
+    *token = BIT;
+    return 1;
+  }
+
+  else if (string[0] == '0') {
+    *token = BIT;
+    return 1;
+  }
+
+  else {
+    /* TODO handle error */
+    return 0;
+  }
+} 
 
 int main(void) {
-  printf("Hello bakoron");
+  Bakoron bakoron;
+  Bakoron_Tree *tree;
+
+  const char *input = "1+0";
+
+  bakoron_register_symbol(&bakoron, EXPRESSION, BK_VARIABLE);
+  bakoron_register_symbol(&bakoron, PLUS, BK_TERMINAL);
+  bakoron_register_symbol(&bakoron, BIT, BK_TERMINAL);
+
+  {
+    Symbol rule[] = {BIT, PLUS, BIT};
+    bakoron_register_rule(&bakoron, EXPRESSION, rule,
+                          sizeof(rule) / sizeof(rule[0]));
+  }
+
+  context.input = input;
+  context.current_index = 0;
+
+  tree = bakoron_parse_string(&bakoron, EXPRESSION, &context, get_next_token);
+
+  bakoron_cleanup(&bakoron);
+  bakoron_cleanup_tree(tree);
+
   return 0;
 }
