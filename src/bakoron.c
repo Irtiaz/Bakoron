a10ffc651e3f64ccb009baf002a6c4d45ade07cd
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2025-09-15 22:01:01 +0600

    Added the terminal parsing function and Parse_Result struct

--- src/bakoron.c
+++ src/bakoron.c
@@ -1,9 +1,9 @@
 #include "bakoron.h"
 
-#include <stdlib.h>
-#include <stdbool.h>
-#include <stdarg.h>
 #include <assert.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
 #include "stb_ds.h"
 
@@ -26,7 +26,12 @@ struct BK_Parser {
   Symbol **symbols;
 };
 
-BK_Tree *tree_term_init(Symbol *term) {
+typedef struct {
+  BK_Tree *tree;
+  size_t end_marker;
+} Parse_Result;
+
+static BK_Tree *tree_term_init(Symbol *term) {
   assert(term->rules == NULL); // assert it is a terminal
   BK_Tree *tree = malloc(sizeof(BK_Tree));
   tree->number_of_children = 0;
@@ -35,7 +40,7 @@ BK_Tree *tree_term_init(Symbol *term) {
   return tree;
 }
 
-BK_Tree *tree_rule_init(Rule *rule) {
+static BK_Tree *tree_rule_init(Rule *rule) {
   BK_Tree *tree = malloc(sizeof(BK_Tree));
   tree->number_of_children = arrlen(rule->rule_body);
   tree->content.children = NULL;
@@ -43,15 +48,47 @@ BK_Tree *tree_rule_init(Rule *rule) {
   return tree;
 }
 
-void tree_add_child(BK_Tree *tree, BK_Tree *child) {
+static void tree_add_child(BK_Tree *tree, BK_Tree *child) {
   assert((size_t)arrlen(tree->content.children) < tree->number_of_children);
   arrput(tree->content.children, child);
+}
+
+static Parse_Result *tree_with_end_marker(BK_Tree *tree, size_t end_marker) {
+  Parse_Result *result = (Parse_Result *)malloc(sizeof(Parse_Result));
+
+  result->tree = tree;
+  result->end_marker = end_marker;
+
+  return result;
+}
+
+static Symbol *get_symbol_from_number(BK_Parser *parser, int number) {
+  for (int i = 0; i < arrlen(parser->symbols); ++i) {
+    if (parser->symbols[i]->number == number)
+      return parser->symbols[i];
+  }
+  return NULL;
+}
+
+// Returns true if added or false if it was already there
+static bool add_symbol(BK_Parser *parser, int symbol_number) {
+  Symbol *symbol = get_symbol_from_number(parser, symbol_number);
+  if (symbol != NULL)
+    return false;
+
+  symbol = (Symbol *)malloc(sizeof(Symbol));
+  symbol->rules = NULL;
+  symbol->number = symbol_number;
+
+  arrput(parser->symbols, symbol);
+  return true;
 }
 
 BK_Parser *bk_parser(void) {
   BK_Parser *parser = (BK_Parser *)malloc(sizeof(BK_Parser));
 
   parser->symbols = NULL;
+  add_symbol(parser, BK_EPSILON);
   return parser;
 }
 
@@ -72,28 +109,9 @@ void bk_parser_destroy(BK_Parser *parser) {
   free(parser);
 }
 
-static Symbol *get_symbol_from_number(BK_Parser *parser, int number) {
-  for (int i = 0; i < arrlen(parser->symbols); ++i) {
-    if (parser->symbols[i]->number == number) return parser->symbols[i];
-  }
-  return NULL;
-}
-
-// Tries to add a symbol and returns true if added or false if it was already there
-static bool add_symbol(BK_Parser *parser, int symbol_number) {
-  Symbol *symbol = get_symbol_from_number(parser, symbol_number);
-  if (symbol != NULL) return false;
-
-  symbol = (Symbol *)malloc(sizeof(Symbol));
-  symbol->rules = NULL;
-  symbol->number = symbol_number;
-  symbol->index = arrlen(parser->symbols);
-
-  arrput(parser->symbols, symbol);
-  return true;
-}
-
-static Rule *create_rule(BK_Parser *parser, Symbol *variable, int rule_descriptor, int *rule_body, int rule_body_length) {
+static Rule *create_rule(BK_Parser *parser, Symbol *variable,
+                         int rule_descriptor, int *rule_body,
+                         int rule_body_length) {
   Rule *rule = (Rule *)malloc(sizeof(Rule));
   rule->variable = variable;
   rule->rule_descriptor = rule_descriptor;
@@ -107,9 +125,11 @@ static Rule *create_rule(BK_Parser *parser, Symbol *variable, int rule_descripto
   return rule;
 }
 
-static void add_rule(BK_Parser *parser, int rule_descriptor, int *rule_raw, int rule_length) {
+static void add_rule(BK_Parser *parser, int rule_descriptor, int *rule_raw,
+                     int rule_length) {
   Symbol *variable = get_symbol_from_number(parser, rule_raw[0]);
-  Rule *rule = create_rule(parser, variable, rule_descriptor, rule_raw + 1, rule_length - 1);
+  Rule *rule = create_rule(parser, variable, rule_descriptor, rule_raw + 1,
+                           rule_length - 1);
 
   arrput(variable->rules, rule);
 }
@@ -141,6 +161,44 @@ void bk_rule_a(BK_Parser *parser, int rule_descriptor, int rule_length,
   }
 
   add_rule(parser, rule_descriptor, rule, rule_length);
+}
+
+bool symbol_is_terminal(Symbol *symbol) { return symbol->rules == NULL; }
+
+static Parse_Result **parse_terminal(Symbol *terminal, size_t parse_from,
+                                     int *tokens) {
+  int token = tokens[parse_from];
+
+  if (terminal->number == BK_EPSILON || terminal->number == token) {
+    Parse_Result **results = NULL;
+
+    BK_Tree *terminal_tree = tree_term_init(terminal);
+    Parse_Result *terminal_result = tree_with_end_marker(
+        terminal_tree,
+        terminal->number == BK_EPSILON ? parse_from : parse_from + 1);
+
+    arrput(results, terminal_result);
+    return results;
+  }
+
+  else
+    return NULL;
+}
+
+static Parse_Result **parse_subrule(Rule *rule, size_t begin, size_t parse_from,
+                                    int *tokens, size_t tokens_length) {}
+
+static Parse_Result **parse_variable(Symbol *variable, size_t parse_from,
+                                     int *tokens, size_t tokens_length) {}
+
+static Parse_Result **parse_recursive(BK_Parser *parser, Symbol *symbol,
+                                      size_t parse_from, int *tokens,
+                                      size_t tokens_length) {
+  if (symbol_is_terminal(symbol)) {
+    return parse_terminal(symbol, parse_from, tokens);
+  } else {
+    return parse_variable(symbol, parse_from, tokens, tokens_length);
+  }
 }
 
 BK_Tree *bk_tree(BK_Parser *parser, int start_symbol, int *tokens,
