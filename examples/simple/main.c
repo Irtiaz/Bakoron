3a82b1c851623fdc50281603b6d3208ade1fc2c4
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2025-09-15 13:17:01 +0600

    Added the API for bakoron parser

--- examples/simple/main.c
+++ examples/simple/main.c
@@ -1,6 +1,49 @@
+#include "bakoron.h"
+#include <assert.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define LOG_ERROR(format, ...)                                                 \
+  fprintf(stderr, __FILE__ ":%d:" format, __LINE__, __VA_ARGS__)
+
+enum { EXPRESSION, NUMBER, PLUS };
+
+int evaluate_tree(BK_Tree *tree, char **lexemes) {
+  if (tree->symbol == EXPRESSION) {
+    assert(tree->number_of_children == 3);
+
+    return evaluate_tree(tree->content.children[0], lexemes) +
+           evaluate_tree(tree->content.children[2], lexemes);
+  }
+
+  else if (tree->symbol == NUMBER) {
+    assert(tree->number_of_children == 0);
+
+    char *lexeme = lexemes[tree->content.index];
+    int value = atoi(lexeme);
+
+    return value;
+  }
+
+  LOG_ERROR("can't evaluate symbol that is neither EXPRESSION nor NUMBER - %d\n", tree->symbol);
+  exit(1);
+}
 
 int main(void) {
-  printf("Hello world\n");
+  BK_Parser *parser = bk_parser();
+
+  bk_rule(parser, 4, EXPRESSION, NUMBER, PLUS, NUMBER);
+
+  int token[] = {NUMBER, PLUS, NUMBER};
+  char *lexemes[] = {"12", "+", "3"};
+
+  BK_Tree *tree =
+      bk_tree(parser, EXPRESSION, token, sizeof(token) / sizeof(token[0]));
+
+  int result = evaluate_tree(tree, lexemes);
+  printf("Result: %d\n", result);
+
+  bk_tree_destroy(tree);
+  bk_parser_destroy(parser);
   return 0;
 }
--- include/bakoron/bakoron.h
+++ include/bakoron/bakoron.h
@@ -1,5 +1,32 @@
 #ifndef BAKORON_H_43543
 #define BAKORON_H_43543
 
+#include <stdio.h>
+
+typedef struct BK_Parser BK_Parser;
+
+typedef struct BK_Tree BK_Tree;
+
+struct BK_Tree {
+  int symbol;
+
+  union {
+    BK_Tree **children;
+    size_t index;
+  } content;
+
+  size_t number_of_children;
+};
+
+BK_Parser *bk_parser(void);
+void bk_parser_destroy(BK_Parser *parser);
+
+void bk_rule(BK_Parser *parser, int rule_length, ...);
+void bk_rule_v(BK_Parser *parser, int rule_length, va_list rule);
+void bk_rule_a(BK_Parser *parser, int rule_length, int *rule);
+
+BK_Tree *bk_tree(BK_Parser *parser, int start_symbol, int *tokens, size_t tokens_length);
+void bk_tree_destroy(BK_Tree *tree);
+
 #endif
 
